745f471ee62eae6f9cb0a7d83b4516246814eecf
oinelsonjunior <oi.nelson.junior@gmail.com>
2026-05-18 16:45:58 +0000

    feat: admin, tipos-servico, seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,15 @@ import { ServicoModule } from './servicos/servico.module';
 import { ChatModule } from './chat/chat.module';
 import { PagamentoModule } from './pagamentos/pagamento.module';
 import { EnderecoModule } from './enderecos/endereco.module';
+import { TipoServicoModule } from './tipos-servico/tipo-servico.module';
+import { AdminModule } from './admin/admin.module';
 import { User } from './users/user.entity';
 import { Servico } from './servicos/servico.entity';
 import { Mensagem } from './chat/mensagem.entity';
 import { Pagamento } from './pagamentos/pagamento.entity';
 import { Endereco } from './enderecos/endereco.entity';
+import { TipoServico } from './tipos-servico/tipo-servico.entity';
+import { TipoServicoService } from './tipos-servico/tipo-servico.service';
 
 @Module({
   imports: [
@@ -20,18 +24,27 @@ import { Endereco } from './enderecos/endereco.entity';
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
         url: config.get('DATABASE_URL'),
-        entities: [User, Servico, Mensagem, Pagamento, Endereco],
+        entities: [User, Servico, Mensagem, Pagamento, Endereco, TipoServico],
         synchronize: true,
         ssl: { rejectUnauthorized: false },
         logging: false,
       }),
       inject: [ConfigService],
     }),
+    TypeOrmModule.forFeature([TipoServico]),
     AuthModule,
     ServicoModule,
     ChatModule,
     PagamentoModule,
     EnderecoModule,
+    TipoServicoModule,
+    AdminModule,
   ],
+  providers: [TipoServicoService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private tipoServicoService: TipoServicoService) {}
+  async onModuleInit() {
+    await this.tipoServicoService.seed();
+  }
+}
